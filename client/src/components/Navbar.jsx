eae368947c150ac9db2178c6d896bed543268033
Cristian Unigarro <ungcrisfit@gmail.com>
2023-02-04 18:13:58 -0500

    add: Footer and LandingScreen

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,122 +13,107 @@ import {
   useColorMode,
   CloseButton,
 } from "@chakra-ui/react";
-import { Link as ReactLink } from 'react-router-dom';
-import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Link as ReactLink } from "react-router-dom";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 // import { ImStatsBars } from 'react-icons/im';
-import { ImStatsBars} from 'react-icons/im'
-
+import { ImStatsBars } from "react-icons/im";
+import { useState } from "react";
 
 const links = [
-    {linkName: 'Cursos', path: '/products'},
-    {linkName: 'Carrito', path: '/cart'}
-]
+  { linkName: "Cursos", path: "/products" },
+  { linkName: "Carrito", path: "/cart" },
+];
 
 const NavLink = ({ path, children }) => (
-    <Link
-        as={ReactLink}
-        to={path}
-        px={2}
-        py={2}
-        rounded='md'
-        _hover={{ textDecoration: 'none', bg: useColorModeValue('gray.200', 'gray.700') }}
-    >    
-        {children}
-    </Link>
-)
+  <Link
+    as={ReactLink}
+    to={path}
+    px={2}
+    py={2}
+    rounded="md"
+    _hover={{ textDecoration: "none", bg: useColorModeValue("gray.200", "gray.700") }}
+  >
+    {children}
+  </Link>
+);
 
 const Navbar = () => {
-    const { isOpen, onClose, onOpen } = useDisclosure();
-    const { colorMode, toggleColorMode} = useColorMode();
-    return (
-        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-            <Flex h={16} alignItems='center' justifyContent='space-between'>
-                <IconButton 
-                    size='md'
-                    icon={isOpen ? <CloseIcon/> : <HamburgerIcon />}
-                    display={{ md: 'none'}}
-                    onClick={isOpen ? onClose : onOpen}
-                />
-                <HStack>
-                    <Link as={ReactLink} to='/'>
-                        <Flex alignItems='center'>
-                            <Icon as={ImStatsBars} h={6} w={6} color='orange.400'/>
-                            <Text fontWeight='extrabold'>Trading</Text>
-                        </Flex>
-                    </Link>
-                    <HStack as='nav' spacing={4} display={{ base: 'none', md: 'flex' }}>
-                        {links.map((link) => (
-                            <NavLink key={link.linkName} path={link.path}>
-                                {link.linkName}
-                            </NavLink>
-                        ))}
-                    </HStack>
-                </HStack>
-                <Flex  alignItems='center'>
-                    <Link>
-                        <Icon
-                            as={colorMode === 'light' ? MoonIcon : SunIcon}
-                            alignSelf='center'
-                            onClick={() => toggleColorMode()}
-                        />
-                    </Link>
-                    <Button 
-                        as={ReactLink}
-                        to='/login'
-                        p={2}
-                        fontSize='sm'
-                        fontWeight={400}
-                        variant='link'
-                        >
-                        Iniciar Sesión
-                    </Button>
-                    <Button 
-                        as={ReactLink}
-                        to='/register'
-                        m={2}
-                        display={{ base: 'none', md: 'inline-flex' }}
-                        fontSize='sm'
-                        fontWeight={600}
-                        _hover={{ bg: 'orange.400' }}
-                        bg='orange.500'
-                        color='white'
-                        >
-                        Registrarse
-                    </Button>
-                </Flex>
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [isHovering, setIsHovering] = useState(false);
+
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <IconButton
+          size="md"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack>
+          <Link
+            as={ReactLink}
+            to="/"
+            style={{ textDecoration: "none" }}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+          >
+            <Flex alignItems="center">
+              <Icon as={ImStatsBars} h={6} w={6} mr={3} color={isHovering ? 'cyan.400' : 'orange.400'} />
+              <Text fontWeight="extrabold">Trading</Text>
             </Flex>
-            {isOpen ? (
-                <Box pb={4} display={{ md: 'none' }}>
-                    <Stack as='nav' spacing={4}>
-                        {links.map((link) => (
-                            <NavLink key={link.linkName} path={link.path}>
-                                {link.linkName}
-                            </NavLink>
-                        ))}
-                        <NavLink key='sign up' path='/register'>
-                            Registrarse
-                        </NavLink>
-                    </Stack>
-                </Box>
-            ) : null}
+          </Link>
+          <HStack as="nav" spacing={4} display={{ base: "none", md: "flex" }}>
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems="center">
+          <Link>
+            <Icon
+              as={colorMode === "light" ? MoonIcon : SunIcon}
+              alignSelf="center"
+              onClick={() => toggleColorMode()}
+            />
+          </Link>
+          <Button as={ReactLink} to="/login" p={2} fontSize="sm" fontWeight={400} variant="link">
+            Iniciar Sesión
+          </Button>
+          <Button
+            as={ReactLink}
+            to="/register"
+            m={2}
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize="sm"
+            fontWeight={600}
+            _hover={{ bg: "orange.400" }}
+            bg="orange.500"
+            color="white"
+          >
+            Registrarse
+          </Button>
+        </Flex>
+      </Flex>
+      {isOpen ? (
+        <Box pb={4} display={{ md: "none" }}>
+          <Stack as="nav" spacing={4}>
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+            <NavLink key="sign up" path="/register">
+              Registrarse
+            </NavLink>
+          </Stack>
         </Box>
-  )
+      ) : null}
+    </Box>
+  );
 };
 
 export default Navbar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
