30dd790e5f1822d698278d345fb680fff5012ad9
Cristian Unigarro <ungcrisfit@gmail.com>
2023-02-04 08:58:57 -0500

    add: loading spinner and error message

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,8 +1,18 @@
-import { Center, Wrap, WrapItem } from '@chakra-ui/react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ProductCard from '../components/ProductCard';
-import { getProducts } from '../redux/actions/productActions';
+import {
+  Center,
+  Wrap,
+  WrapItem,
+  Spinner,
+  Stack,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+  AlertTitle,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductCard from "../components/ProductCard";
+import { getProducts } from "../redux/actions/productActions";
 
 const ProductsScreen = () => {
   const dispatch = useDispatch();
@@ -15,16 +25,28 @@ const ProductsScreen = () => {
   }, [dispatch]);
 
   return (
-    <Wrap spacing='30px' justify='center' minHeight='100vh'>
-      {products.map((product) => (
-        <WrapItem key={product._id}>
-            <Center w='250px' h='550px'>
-                <ProductCard product={product}/>
+    <Wrap spacing="30px" justify="center" minHeight="100vh">
+      {loading ? (
+        <Stack direction="row" spacing={4}>
+          <Spinner mt={20} thickness='2px' speed="0.65s" emptyColor="gray.200" color="orange.500" size="xl" />
+        </Stack>
+      ) : error ? (
+        <Alert status='error'>
+          <AlertIcon />
+          <AlertTitle>¡Lo sentimos!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      ) : (
+        products.map((product) => (
+          <WrapItem key={product._id}>
+            <Center w="250px" h="550px">
+              <ProductCard product={product} />
             </Center>
-        </WrapItem>
-      ))}
+          </WrapItem>
+        ))
+      )}
     </Wrap>
-  )
-}
+  );
+};
 
-export default ProductsScreen
+export default ProductsScreen;
