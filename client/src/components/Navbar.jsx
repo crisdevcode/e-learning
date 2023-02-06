3f39b4ada04d624ca3840c26ee843f3de0379e8f
Cristian Unigarro <ungcrisfit@gmail.com>
2023-02-06 02:13:04 -0500

    add: admin console - users

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ import {
 import { Link as ReactLink } from "react-router-dom";
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { CgProfile } from "react-icons/cg";
-import { MdLocalShipping, MdLogout } from "react-icons/md";
+import { MdLocalShipping, MdLogout, MdOutlineAdminPanelSettings } from "react-icons/md";
 import { FiShoppingCart } from 'react-icons/fi';
 import { ImStatsBars } from "react-icons/im";
 import { useState } from "react";
@@ -126,6 +126,15 @@ const Navbar = () => {
                   <MdLocalShipping />
                   <Text ml="2">Mis Cursos</Text>
                 </MenuItem>
+                {userInfo.isAdmin === 'true' && (
+                  <>
+                    <MenuDivider />
+                    <MenuItem as={ReactLink} to={'/admin-console'}>
+                      <MdOutlineAdminPanelSettings />
+                      <Text ml='2'>{'  ⭐ Admin ⭐'}</Text>
+                    </MenuItem>
+                  </>
+                )}
                 <MenuDivider />
                 <MenuItem onClick={logoutHandler}>
                   <MdLogout />
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,12 +3,14 @@ import products from "./slices/productsSlice.js";
 import cart from "./slices/cartSlice.js";
 import user from "./slices/userSlice.js";
 import order from "./slices/orderSlice.js";
+import admin from "./slices/adminSlice.js";
 
 const reducer = combineReducers({
   products,
   cart,
   user,
   order,
+  admin,
 });
 
 export default configureStore({
